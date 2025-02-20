fa9ed5e62b38f06ed914df8e8124fe0551232847
MeowX-2 <alamarifa9@gmail.com>
2025-02-20 17:44:56 +0600

    update

--- lua/plugins/tresitter.lua
+++ lua/plugins/tresitter.lua
@@ -7,7 +7,7 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
-        -- additional_vim_regex_highlighting = { "latex", "markdown" },
+        additional_vim_regex_highlighting = { "latex", "markdown" },
         -- npm install -g tree-sitter-cli
       },
       indent = { enable = true },
