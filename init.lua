de976bbbd15a1fff5924b58d303749b8d72a33b6
MeowX-2 <alamarifa9@gmail.com>
2025-05-06 03:01:45 +0600

    new plugin

--- init.lua
+++ init.lua
@@ -3,18 +3,19 @@ vim.opt.termguicolors = true
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
+require("autoclose").setup()
 require("lspconfig").pyright.setup({})
 -------------------------
 vim.cmd("colorscheme duskfox")
