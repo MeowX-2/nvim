4ac6284caeb9b8c90bb45c294fbf516d7bf9bd0c
MeowX-2 <alamarifa9@gmail.com>
2026-01-13 19:15:12 +0600

    organize

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,51 +1,64 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "texlab", "eslint", "html", "pylsp", "pyright", "ts_ls", "denols", "cssls", "stylua" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"texlab",
+					"eslint",
+					"html",
+					"pylsp",
+					"pyright",
+					"ts_ls",
+					"denols",
+					"cssls",
+					"stylua",
+					"clangd",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "williamboman/mason-lspconfig.nvim" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local servers = { "lua_ls", "texlab", "eslint", "html", "pylsp", "pyright", "ts_ls", "denols", "cssls" }
-      for _, server_name in ipairs(servers) do
-        vim.lsp.config(server_name, {
-          capabilities = capabilities,
-        })
-        vim.lsp.enable(server_name)
-      end
+			local servers =
+				{ "lua_ls", "texlab", "eslint", "html", "pylsp", "pyright", "ts_ls", "denols", "cssls", "clangd" }
+			for _, server_name in ipairs(servers) do
+				vim.lsp.config(server_name, {
+					capabilities = capabilities,
+				})
+				vim.lsp.enable(server_name)
+			end
 
-      -- Special configuration for pylsp
-      vim.lsp.config('pylsp', {
-        capabilities = capabilities,
-        settings = {
-          pylsp = {
-            plugins = {
-              pycodestyle = { enabled = false },
-              pydocstyle = { enabled = false },
-              mccabe = { enabled = false },
-            },
-          },
-        },
-      })
+			-- Special configuration for pylsp
+			vim.lsp.config("pylsp", {
+				capabilities = capabilities,
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = { enabled = false },
+							pydocstyle = { enabled = false },
+							mccabe = { enabled = false },
+						},
+					},
+				},
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set({ "n" }, "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set({ "n" }, "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
