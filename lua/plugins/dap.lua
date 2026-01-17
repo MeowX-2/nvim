7ae4cfe2ba9e45465adb7549e6a6d91c24b4af95
MeowX-2 <alamarifa9@gmail.com>
2026-01-17 15:48:15 +0600

    debuggers

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,35 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("mason-nvim-dap").setup({
+      ensure_installed = { "codelldb", "python" },
+      handlers = {},
+    })
+
+    dapui.setup()
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+  end,
+}
