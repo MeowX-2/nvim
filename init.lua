2c051b1a0a7b814b9092389eaa119e736cfacb79
MeowX-2 <alamarifa9@gmail.com>
2026-01-24 13:39:09 +0600

    newtheme

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 -------------------------
-vim.cmd("colorscheme ofirkai")
+vim.cmd("colorscheme kanagawa-wave")
 vim.cmd("se cursorline")
 local opts = {}
 
