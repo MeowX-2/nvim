944b16dfe42dd469b870f0136a878954db6c8f77
MeowX2 <alamarifa9@gmail.com>
2026-08-06 00:50:18 +0600

    fix: update lspconfig for neovim 0.11+ and ignore compiled parsers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,30 +29,51 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = { "williamboman/mason-lspconfig.nvim" },
 		config = function()
-			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 			local servers =
 				{ "lua_ls", "texlab", "eslint", "html", "pyright", "ts_ls", "denols", "cssls", "clangd" }
-			for _, server_name in ipairs(servers) do
-				lspconfig[server_name].setup({
+
+			if vim.lsp.config then
+				vim.lsp.config("*", {
 					capabilities = capabilities,
 				})
-			end
+				for _, server_name in ipairs(servers) do
+					vim.lsp.enable(server_name)
+				end
 
-			-- Special configuration for pylsp
-			lspconfig.pylsp.setup({
-				capabilities = capabilities,
-				settings = {
-					pylsp = {
-						plugins = {
-							pycodestyle = { enabled = false },
-							pydocstyle = { enabled = false },
-							mccabe = { enabled = false },
+				vim.lsp.config("pylsp", {
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = { enabled = false },
+								pydocstyle = { enabled = false },
+								mccabe = { enabled = false },
+							},
 						},
 					},
-				},
-			})
+				})
+				vim.lsp.enable("pylsp")
+			else
+				local lspconfig = require("lspconfig")
+				for _, server_name in ipairs(servers) do
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end
+
+				lspconfig.pylsp.setup({
+					capabilities = capabilities,
+					settings = {
+						pylsp = {
+							plugins = {
+								pycodestyle = { enabled = false },
+								pydocstyle = { enabled = false },
+								mccabe = { enabled = false },
+							},
+						},
+					},
+				})
+			end
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
