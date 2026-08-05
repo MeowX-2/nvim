c6f5c8a9d6a4fc6ccf218cf81a338fc3b95676dd
MeowX2 <alamarifa9@gmail.com>
2026-08-06 01:07:37 +0600

    feat: add NvChad-style dashboard, new file prompt, and Telescope/NvimTree file opener keymaps

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,20 +1,31 @@
 return {
-    'goolord/alpha-nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        --require'alpha'.setup(require'alpha.themes.startify'.config)
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.startify")
- --        dashboard.section.header.val ={
- -- [[                                                                                 ]],
- -- [[                                                                                 ]],
- -- [[  ██╗   ██╗ ██████╗     ███╗   ███╗███████╗ ██████╗ ██╗    ██╗██╗  ██╗██████╗ ██╗]],
- -- [[  ╚██╗ ██╔╝██╔═══██╗    ████╗ ████║██╔════╝██╔═══██╗██║    ██║╚██╗██╔╝╚════██╗██║]],
- -- [[   ╚████╔╝ ██║   ██║    ██╔████╔██║█████╗  ██║   ██║██║ █╗ ██║ ╚███╔╝  █████╔╝██║]],
- -- [[    ╚██╔╝  ██║   ██║    ██║╚██╔╝██║██╔══╝  ██║   ██║██║███╗██║ ██╔██╗ ██╔═══╝ ╚═╝]],
- -- [[     ██║   ╚██████╔╝    ██║ ╚═╝ ██║███████╗╚██████╔╝╚███╔███╔╝██╔╝ ██╗███████╗██╗]],
- -- [[     ╚═╝    ╚═════╝     ╚═╝     ╚═╝╚══════╝ ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═╝]],
- --    }
-    alpha.setup(dashboard.opts)
-    end,
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
+
+		dashboard.section.header.val = {
+			"                            ",
+			"   ███╗   ██╗██╗   ██╗██╗███╗   ███╗   ",
+			"   ████╗  ██║██║   ██║██║████╗ ████║   ",
+			"   ██╔██╗ ██║██║   ██║██║██╔████╔██║   ",
+			"   ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║   ",
+			"   ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║   ",
+			"   ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝   ",
+			"                            ",
+		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button("f", "🔍  Find file", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("n", "📄  New file", "<cmd>lua vim.keymap.get('n', '/fn').callback()<CR>"),
+			dashboard.button("r", "🕒  Recent files", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("g", "🔎  Find text", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("c", "⚙️   Configuration", "<cmd>e $MYVIMRC<CR>"),
+			dashboard.button("q", "🚪  Quit Neovim", "<cmd>qa<CR>"),
+		}
+
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+	end,
 }
